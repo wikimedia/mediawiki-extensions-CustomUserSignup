95691d1c95481d1488546bce12019b4d305934b1
Paladox <thomasmulhall410@yahoo.com>
2016-03-07 18:22:51 +0000

    Add npm entry point
    Change-Id: I9f32d59ed127e11adecd996f731f138265cfb97b

--- modules/AccountCreationUserBucket.js
+++ modules/AccountCreationUserBucket.js
@@ -1,8 +1,10 @@
+/*jshint -W069 */
+
 //checks
-if(!mw.activeCampaigns){ mw.activeCampaigns ={}; }
+if(!mediaWiki.activeCampaigns){ mediaWiki.activeCampaigns ={}; }
 
 //define new active campaign
-mw.activeCampaigns.AccountCreation =
+mediaWiki.activeCampaigns.AccountCreation =
 
 { 
   //Treatment name
@@ -29,62 +31,62 @@ mw.activeCampaigns.AccountCreation =
   
   "ACP1": function(){
 	  //change to NiceMsg1 campaign
-	  $j("#pt-anonlogin a").each(function(){
-		  $j(this).attr("href", $j(this).attr("href") + "&campaign=ACP1" ); 
+	  jQuery("#pt-anonlogin a").each(function(){
+		  jQuery(this).attr("href", jQuery(this).attr("href") + "&campaign=ACP1" ); 
 	  });
-	  $j("#pt-login a").each(function(){
-		  $j(this).attr("href", $j(this).attr("href") + "&campaign=ACP1" ); 
+	  jQuery("#pt-login a").each(function(){
+		  jQuery(this).attr("href", jQuery(this).attr("href") + "&campaign=ACP1" ); 
 	  });
 	  
   },
   "ACP2": function(){
 	  //change to NiceMsg2 campaign
-	  $j("#pt-anonlogin a").each(function(){
-		  $j(this).attr("href", $j(this).attr("href") + "&campaign=ACP2" ); 
+	  jQuery("#pt-anonlogin a").each(function(){
+		  jQuery(this).attr("href", jQuery(this).attr("href") + "&campaign=ACP2" ); 
 	  });
-	  $j("#pt-login a").each(function(){
-		  $j(this).attr("href", $j(this).attr("href") + "&campaign=ACP2" ); 
+	  jQuery("#pt-login a").each(function(){
+		  jQuery(this).attr("href", jQuery(this).attr("href") + "&campaign=ACP2" ); 
 	  });
   },
 
   "ACP3": function(){
 	  //change to NiceMsg2 campaign
-	  $j("#pt-anonlogin a").each(function(){
-		  $j(this).attr("href", $j(this).attr("href") + "&campaign=ACP3" ); 
+	  jQuery("#pt-anonlogin a").each(function(){
+		  jQuery(this).attr("href", jQuery(this).attr("href") + "&campaign=ACP3" ); 
 	  });
-	  $j("#pt-login a").each(function(){
-		  $j(this).attr("href", $j(this).attr("href") + "&campaign=ACP3" ); 
+	  jQuery("#pt-login a").each(function(){
+		  jQuery(this).attr("href", jQuery(this).attr("href") + "&campaign=ACP3" ); 
 	  });
   },
   
   // "allActive" is reserved.
   // If this function exists, it will be apply to every user not in the "none" bucket
   "allActive": function(){
-	if ( !mw.config.get( 'wgCustomUserSignupTrackAccountCreations' ) ) { return; }
+	if ( !mediaWiki.config.get( 'wgCustomUserSignupTrackAccountCreations' ) ) { return; }
 		   
 		  //track account creation attempts
-		  $j("#wpCreateaccount").click(function(){ $j.trackAction('submit-signup-data'); });
+		  jQuery("#wpCreateaccount").click(function(){ jQuery.trackAction('submit-signup-data'); });
 		  
 		  //this is the "don't have an account? CREATE ONE" link
-		  $j("#userloginlink").click(function(){ $j.trackAction('visit-signup'); });
+		  jQuery("#userloginlink").click(function(){ jQuery.trackAction('visit-signup'); });
 		  
-	if($j.cookie('acctcreation') ){
+	if(jQuery.cookie('acctcreation') ){
 		  //add click tracking to preview
-		  $j("#wpPreview").click(function(){ $j.trackAction('preview'); });
+		  jQuery("#wpPreview").click(function(){ jQuery.trackAction('preview'); });
 		  
 		  //add click tracking to save
-		  $j("#wpSave").click(function(){ $j.trackAction('save'); });
+		  jQuery("#wpSave").click(function(){ jQuery.trackAction('save'); });
 	  }
   },
 	  
   "all": function(){
 			//add up all rates
-			var campaign = mw.activeCampaigns.AccountCreation;
-		  	if( $j.cookie('userbuckets') && 
-		  			$j.parseJSON( $j.cookie('userbuckets') )["AccountCreation"]){
-		  		var buckets = $j.parseJSON( $j.cookie('userbuckets') );
+			var campaign = mediaWiki.activeCampaigns.AccountCreation;
+		  	if( jQuery.cookie('userbuckets') && 
+		  			jQuery.parseJSON( jQuery.cookie('userbuckets') )["AccountCreation"]){
+		  		var buckets = jQuery.parseJSON( jQuery.cookie('userbuckets') );
 		  		
-		  		if(typeof(campaign[buckets[campaign.name][0]]) == "function"){
+		  		if(typeof(campaign[buckets[campaign.name][0]]) === "function"){
 		  			campaign[buckets[campaign.name][0]](); //function to execute
 					campaign.allActive();
 					return;
@@ -105,7 +107,7 @@ mw.activeCampaigns.AccountCreation =
 				for( rate in campaign.rates ){
 					next_val += campaign.rates[rate];
 					if(prev_val <= currentUser && currentUser < next_val){
-						if(rate != "none"){
+						if(rate !== "none"){
 							campaign[rate]();
 						}
 						break;
